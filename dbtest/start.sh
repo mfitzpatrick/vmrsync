2d2eba663b8c1e883e1c87cdb65e2fb5dcadb5d0
Matt Fitzpatrick <1657422+mfitzpatrick@users.noreply.github.com>
2022-05-27 18:09:02 +1000

    CI: try disabling the PTTY when setting up schema
    - The last error complained that no TTY was available. The -T option
      should stop the allocation

--- dbtest/start.sh
+++ dbtest/start.sh
@@ -6,8 +6,8 @@ docker-compose up -d
 
 sleep 5
 
-docker-compose exec db /usr/local/firebird/bin/isql -u SYSDBA -i /setup/schema.sql
-docker-compose exec db /usr/local/firebird/bin/isql -u SYSDBA -i /setup/initial-data.sql
+docker-compose exec -T db /usr/local/firebird/bin/isql -u SYSDBA -i /setup/schema.sql
+docker-compose exec -T db /usr/local/firebird/bin/isql -u SYSDBA -i /setup/initial-data.sql
 
 echo "Firebird DB is ready for operations on port 3050"
 
