0b2a274c0c29ac1ecda6ce7ef8aec753dbcac1a6
Matt Fitzpatrick <1657422+mfitzpatrick@users.noreply.github.com>
2022-10-10 22:40:45 +1000

    Fix integration test errors
    - The integration tests were failing because the config file integration
      tests were trying to process the config file before the flags had been
      parsed. Removing that init function and allowing TestMain to handle it
      all resolves the issue.
    - Also update our build constraint definitions to the modern method

--- src/config_integ_test.go
+++ src/config_integ_test.go
@@ -1,21 +1,13 @@
-// +build integration
+//go:build integration
 
 package main
 
 import (
-	"log"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func init() {
-	// For integration tests, we need to read the configuration file
-	if err := parseConfig(configFilePath); err != nil {
-		log.Fatalf("Config parsing failed: %v", err)
-	}
-}
 
 func TestParseConfig(t *testing.T) {
 	assert.Equal(t, "no-api-key", tripwatchAPIkey)
